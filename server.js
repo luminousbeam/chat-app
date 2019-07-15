37a890207e61e3c8ac1b5320712b8422f58f46af
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-15 07:26:16 -0400

    install body-parser and set up as middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var server = app.listen(3000, () => {
   console.log('server is listening on port ', server.address().port)
