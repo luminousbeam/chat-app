9f6dbc5071b99b37ce99ba4a9bce4d08fc499b76
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-15 08:31:12 -0400

    move Jquery script to separate JS file

---
+++ myscript.js
@@ -0,0 +1,21 @@
+$(() => {
+  $("#send").click(() => {
+    var message = { name: $("#name").val(), message: $("#message").val() }
+    postMessage(message)
+  })
+  getMessages()
+})
+
+function addMessages(message){
+  $("#messages").append(`<h4> ${message.name} </h4> <p> ${message.message} </p>`)
+}
+
+function getMessages(){
+  $.get('http://localhost:3000/messages', (data) => {
+    data.forEach(addMessages)
+  })
+}
+
+function postMessage(message){
+  $.post('http://localhost:3000/messages', message)
+}
