ec821897a9268edb5f15e0c1a0eb3336ba07ddc7
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-15 08:25:48 -0400

    add ability to post new messages upon refresh

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+var messages = [
+  {name: "Joe", message: "howdy"},
+  {name: "Susan", message: "hai"}
+]
+
+app.get('/messages', (req, res) => {
+  res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+  messages.push(req.body)
+  res.sendStatus(200)
+})
+
 var server = app.listen(3000, () => {
   console.log('server is listening on port ', server.address().port)
 })
