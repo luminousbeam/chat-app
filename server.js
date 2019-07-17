956b11cf3c2ad32d51a5cafeaca4d643fb2f6c19
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-17 13:21:49 -0400

    set up mongoose connection to MongoDB

--- server.js
+++ server.js
@@ -3,28 +3,43 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-  {name: "Joe", message: "howdy"},
-  {name: "Susan", message: "hai"}
-]
+var dbUrl = 'mongodb+srv://DB_USER:DB_PW@cluster0-kdi3p.mongodb.net/test?retryWrites=true&w=majority'
+
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String
+})
 
 app.get('/messages', (req, res) => {
-  res.send(messages)
+  Message.find({}, (err,messages) => {
+    res.send(messages)
+  })
 })
 
 app.post('/messages', (req, res) => {
-  messages.push(req.body)
-  io.emit('message', req.body)
-  res.sendStatus(200)
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if(err)
+      sendStatus(500)
+
+      io.emit('message', req.body)
+      res.sendStatus(200)
+  })
 })
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
+  console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
