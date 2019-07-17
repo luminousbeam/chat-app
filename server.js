cb2244927395ceff1d6de1ea1a8279b313612192
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-17 13:27:03 -0400

    had to uninstall and reinstall mongoose to fix issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var dbUrl = 'mongodb+srv://DB_USER:DB_PW@cluster0-kdi3p.mongodb.net/test?retryWrites=true&w=majority'
+var dbUrl = 'mongodb+srv://luminous:pw1234@cluster0-kdi3p.mongodb.net/test?retryWrites=true&w=majority'
 
 var Message = mongoose.model("Message", {
   name: String,
